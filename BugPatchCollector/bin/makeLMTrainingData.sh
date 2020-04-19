ae21d018c0c75c84ade71d3d6b0c8a88532ba2aa
eunjiwon <21500420@handong.edu>
2020-04-19 20:22:44 +0900

    add shell scripts

---
+++ BugPatchCollector/bin/makeLMTrainingData.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# incubator-hivemall
+./BugPatchCollector -i https://github.com/apache/incubator-hivemall -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /data/BIC/BIC_incubator-hivemall.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
+# bval
+./BugPatchCollector -i https://github.com/apache/bval -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /data/BIC/BIC_bval.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
+
+# lottie-android
+#./BugPatchCollector -i https://github.com/airbnb/lottie-android -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/BIC_lottie-android.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
+
