ef68c097dc2240896ce01866dee944b5c6782651
Eunji Won <21500420@handong.edu>
2020-04-24 15:52:02 +0900

    change project order

--- BugPatchCollector/bin/makeDPTrainingData.sh
+++ BugPatchCollector/bin/makeDPTrainingData.sh
@@ -7,7 +7,7 @@
 
 # # lottie-android
 # ./BugPatchCollector -i https://github.com/airbnb/lottie-android -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/DP -t -c /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/BIC_lottie-android.csv -s 2017-01-01 00:00:00 -e 2019-06-30 59:59:59 -d 2 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Commit/lottie-android
-for project_name in "camel" "groovy" "jena" "bval" "incubator-hivemall" "ace" "incubator-dolphinscheduler" "ant-ivy" "apex-core" "bigtop" "cayenne" "cordova-android" "creadur-rat" "crunch" "cxf-fediz" "deltaspike" "gora" "guacamole-client";
+for project_name in "jena" "camel" "groovy" "bval" "incubator-hivemall" "ace" "incubator-dolphinscheduler" "ant-ivy" "apex-core" "bigtop" "cayenne" "cordova-android" "creadur-rat" "crunch" "cxf-fediz" "deltaspike" "gora" "guacamole-client";
   do
   ./BugPatchCollector -i https://github.com/apache/${project_name} -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/DP -t -c /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC/BIC_${project_name}.csv -s 2017-01-01 00:00:00 -e 2019-06-30 59:59:59 -d 2 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Commit/${project_name} 
 done
