a430b6cff723beb31b54ca612c548c0bf84e7045
ejwon <21500420@handong.edu>
2020-05-09 18:53:51 +0900

    add dataset

---
+++ TrainData/Commit/makeFolder.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for project_name in "camel" "groovy" "jena" "bval" "incubator-hivemall" "ace" "incubator-dolphinscheduler" "ant-ivy" "apex-core" "bigtop" "cayenne" "cordova-android" "creadur-rat" "crunch" "cxf-fediz" "deltaspike" "gora" "guacamole-client";
+  do
+	if [ ! -d ${project_name} ]; then
+		mkdir ${project_name}
+	fi
+done
