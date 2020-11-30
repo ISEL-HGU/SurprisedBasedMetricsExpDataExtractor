ed7fda1ec1f98442f5f0ddcd5dbab3808a8fb1ba
Eunji Won <21500420@handong.edu>
2020-11-30 20:01:53 +0900

    Update makeBIC.sh

--- BugPatchCollector/bin/makeBIC.sh
+++ BugPatchCollector/bin/makeBIC.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-# ./BugPatchCollector -i https://github.com/apache/ace -j issues.apache.org -k ACE -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/camel -j issues.apache.org -k CAMEL -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-# ./BugPatchCollector -i https://github.com/apache/groovy -j issues.apache.org -k GROOVY -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-#./BugPatchCollector -i https://github.com/apache/jena -j issues.apache.org -k JENA -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/bval -j issues.apache.org -k BVAL -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/incubator-hivemall -j issues.apache.org -k HIVEMALL -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-#./BugPatchCollector -i https://github.com/apache/myfaces -j issues.apache.org -k MYFACES -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-#./BugPatchCollector -i https://github.com/apache/incubator-dolphinscheduler -j issues.apache.org -k AIRFLOW -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/ant-ivy -j issues.apache.org -k IVY -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-#./BugPatchCollector -i https://github.com/apache/apex-core -j issues.apache.org -k APEXCORE -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-# ./BugPatchCollector -i https://github.com/apache/bigtop -j issues.apache.org -k BIGTOP -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/cayenne -j issues.apache.org -k CAY -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-# ./BugPatchCollector -i https://github.com/apache/cordova-android -j issues.apache.org -k CB -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-# ./BugPatchCollector -i https://github.com/apache/creadur-rat -j issues.apache.org -k RAT -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-# ./BugPatchCollector -i https://github.com/apache/crunch -j issues.apache.org -k CRUNCH -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-#./BugPatchCollector -i https://github.com/apache/cxf-fediz -j issues.apache.org -k FEDIZ -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/deltaspike -j issues.apache.org -k DELTASPIKE -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/gora -j issues.apache.org -k GORA -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
-./BugPatchCollector -i https://github.com/apache/guacamole-client -j issues.apache.org -k GUACAMOLE -b -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC
+./BugPatchCollector -i https://github.com/apache/jena -j issues.apache.org -k JENA -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/ace -j issues.apache.org -k ACE -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/camel -j issues.apache.org -k CAMEL -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/groovy -j issues.apache.org -k GROOVY -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/bval -j issues.apache.org -k BVAL -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/incubator-hivemall -j issues.apache.org -k HIVEMALL -b -o ../../BIC
+#./BugPatchCollector -i https://github.com/apache/myfaces -j issues.apache.org -k MYFACES -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/incubator-dolphinscheduler -j issues.apache.org -k AIRFLOW -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/ant-ivy -j issues.apache.org -k IVY -b -o ../../BIC
+#./BugPatchCollector -i https://github.com/apache/apex-core -j issues.apache.org -k APEXCORE -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/bigtop -j issues.apache.org -k BIGTOP -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/cayenne -j issues.apache.org -k CAY -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/cordova-android -j issues.apache.org -k CB -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/creadur-rat -j issues.apache.org -k RAT -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/crunch -j issues.apache.org -k CRUNCH -b -o ../../BIC
+#./BugPatchCollector -i https://github.com/apache/cxf-fediz -j issues.apache.org -k FEDIZ -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/deltaspike -j issues.apache.org -k DELTASPIKE -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/gora -j issues.apache.org -k GORA -b -o ../../BIC
+# ./BugPatchCollector -i https://github.com/apache/guacamole-client -j issues.apache.org -k GUACAMOLE -b -o ../../BIC
