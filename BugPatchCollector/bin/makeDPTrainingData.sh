b21decbd2da0670e0c121bfde00095089b67851c
Eunji Won <21500420@handong.edu>
2020-11-30 20:13:31 +0900

    Update makeDPTrainingData.sh

--- BugPatchCollector/bin/makeDPTrainingData.sh
+++ BugPatchCollector/bin/makeDPTrainingData.sh
@@ -1,12 +1,4 @@
-#!/bin/sh
-# # incubator-hivemall
-# ./BugPatchCollector -i https://github.com/apache/incubator-hivemall -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /data/BIC/BIC_incubator-hivemall.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
-# # bval
-# ./BugPatchCollector -i https://github.com/apache/bval -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /data/BIC/BIC_bval.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
-# # lottie-android
-# ./BugPatchCollector -i https://github.com/airbnb/lottie-android -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/BIC_lottie-android.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
 for project_name in "ant-ivy" "bval" "camel" "cayenne" "deltaspike" "gora" "guacamole-client" "incubator-hivemall";
-#for project_name in "jena";
   do
-  ./BugPatchCollector -i https://github.com/apache/${project_name} -o ../../Output/LM -t -c ../../BIC/BIC_${project_name}.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p ../../TrainData
+  ./BugPatchCollector -i https://github.com/apache/${project_name} -o ../../Output/DP -t -c ../../BIC/BIC_${project_name}.csv -s 2017-01-01 00:00:00 -e 2019-09-30 59:59:59 -d 2 -p ../../TrainData/Commit/${project_name}
 done
