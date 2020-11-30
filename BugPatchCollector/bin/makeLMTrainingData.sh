568437a71be0dca1274cec1230f350ac83337bcc
Eunji Won <21500420@handong.edu>
2020-11-30 20:12:19 +0900

    Update makeLMTrainingData.sh

--- BugPatchCollector/bin/makeLMTrainingData.sh
+++ BugPatchCollector/bin/makeLMTrainingData.sh
@@ -8,9 +8,5 @@
 for project_name in "ant-ivy" "bval" "camel" "cayenne" "deltaspike" "gora" "guacamole-client" "incubator-hivemall";
 #for project_name in "jena";
   do
-  ./BugPatchCollector -i https://github.com/apache/${project_name} -o /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM -t -c /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/BIC/BIC_${project_name}.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData
+  ./BugPatchCollector -i https://github.com/apache/${project_name} -o ../../Output/LM -t -c ../../BIC/BIC_${project_name}.csv -s 0000-00-00 00:00:00 -e 2016-12-31 59:59:59 -d 1 -p ../../TrainData
 done
-
-
-
-
